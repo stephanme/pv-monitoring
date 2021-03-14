e8c3a8cc1195d8c7febe860d29c5d73f521c25b7
Stephan Merker <stephan.merker@sap.com>
2021-03-14 21:21:50 +0100

    Deploy into monitoring namespace

--- prometheus/deploy.sh
+++ prometheus/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack --values kube-prometheus-stack-values.yaml
-helm upgrade --install prometheus-lt prometheus-community/kube-prometheus-stack --values kube-prometheus-stack-values-lt.yaml
+helm upgrade --install --create-namespace --namespace monitoring prometheus prometheus-community/kube-prometheus-stack --values kube-prometheus-stack-values.yaml
+helm upgrade --install --namespace monitoring prometheus-lt prometheus-community/kube-prometheus-stack --values kube-prometheus-stack-values-lt.yaml
 
 # upload dashboards
-kubectl apply -f ./grafana-dashboards
+kubectl apply --namespace monitoring -f ./grafana-dashboards
