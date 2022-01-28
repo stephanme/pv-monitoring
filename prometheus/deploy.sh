1bdf7ec2fd9f8e84b364d326afaac9f80832f31c
Stephan Merker <stephan.merker@sap.com>
2022-01-28 18:45:54 +0100

    Update to kube_prometheus_stack 30.2.0

--- prometheus/deploy.sh
+++ prometheus/deploy.sh
@@ -4,14 +4,14 @@ set -ex
 scriptdir=$(dirname "$0")
 
 # helm repo update
-kube_prometheus_stack_version=30.0.0
+kube_prometheus_stack_version=30.2.0
 helm upgrade --install --create-namespace --namespace monitoring prometheus prometheus-community/kube-prometheus-stack --version ${kube_prometheus_stack_version} --values $scriptdir/kube-prometheus-stack-values.yaml
 helm upgrade --install --namespace monitoring prometheus-lt prometheus-community/kube-prometheus-stack --version ${kube_prometheus_stack_version} --values $scriptdir/kube-prometheus-stack-values-lt.yaml
 
 # ingress
-kubectl apply --namespace monitoring -f ingress.yaml
+kubectl apply --namespace monitoring -f $scriptdir/ingress.yaml
 # service for traefik metrics
-kubectl apply --namespace kube-system -f traefik-monitoring.yaml
+kubectl apply --namespace kube-system -f $scriptdir/traefik-monitoring.yaml
 
 # upload dashboards
 kubectl apply --namespace monitoring -f  $scriptdir/grafana-dashboards
