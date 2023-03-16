96fea416a8132993c5ca5aed1885cb20b140ac3f
Stephan Merker <stephan.merker@sap.com>
2023-03-16 20:29:34 +0100

    Traefik metrics and dashboard
    - consolidate traefik "stuff" in k3s dir
    - update grafana dashboard
    - fix service monitor config to work with dashboard

--- k3s/deploy.sh
+++ k3s/deploy.sh
@@ -3,6 +3,12 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-# manifests shall specify namespace
+# namespace: system-upgrade
 kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/v0.10.0/system-upgrade-controller.yaml
-kubectl apply -f $scriptdir
+kubectl apply --namespace system-upgrade -f $scriptdir/k3s-upgrade.yaml
+
+# namespace: kube-system
+for f in $scriptdir/traefik*.yaml
+do
+    kubectl apply --namespace kube-system -f $f
+done
