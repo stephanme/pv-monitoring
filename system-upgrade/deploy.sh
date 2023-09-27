099e67c990f676721df30e0e838838374ce1aa6c
Stephan Merker <stephan.merker@sap.com>
2023-09-27 19:05:08 +0200

    Update k3s to v1.28.2+k3s1
    - and upgrade-controller to 0.13.1

--- system-upgrade/deploy.sh
+++ system-upgrade/deploy.sh
@@ -3,5 +3,5 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/v0.10.0/system-upgrade-controller.yaml
+kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/v0.13.1/system-upgrade-controller.yaml
 kubectl apply --namespace system-upgrade -f $scriptdir/k3s-upgrade.yaml
