91bb6a9a6488d1b8e30225e6d20d2514a10193c5
Stephan Merker <stephan.merker@sap.com>
2023-10-14 16:28:09 +0200

    Refactored kube-system namespace
    - every app in own dir
    - use HelmChart kind to install purelb helm chart

--- kube-system/deploy.sh
+++ kube-system/deploy.sh
@@ -3,16 +3,6 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-# traefik
-for f in $scriptdir/traefik*.yaml
-do
-    kubectl apply --namespace kube-system -f $f
-done
-
-# purelb
-# assumes that helm repo was updated
-purelb_version=0.7.1
-helm upgrade --install --namespace kube-system purelb purelb/purelb --version ${purelb_version} --values $scriptdir/purelb-values.yaml
-
-# dnsmasq
-kubectl apply --namespace kube-system -f $scriptdir/dnsmasq.yaml
+$scriptdir/traefik/deploy.sh
+$scriptdir/purelb/deploy.sh
+$scriptdir/dnsmasq/deploy.sh
---
+++ kube-system/dnsmasq/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+
+scriptdir=$(dirname "$0")
+kubectl apply -f $scriptdir
