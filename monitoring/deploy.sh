bcc517380a2d340c026a3dafc0f389b20871d5f4
Stephan Merker <merker.stephan@gmail.com>
2023-10-16 17:17:41 +0200

    Use HelmChart kind to install prometheus
    - update CRDs via prometheus-operator-crds chart
    - avoids scripting in deploy.sh and relying on updated local helm repos
    - extra namespace.yaml to create namespace

--- monitoring/deploy.sh
+++ monitoring/deploy.sh
@@ -3,6 +3,8 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
+kubectl apply -f $scriptdir/namespace.yaml
+
 $scriptdir/prometheus/deploy.sh
 $scriptdir/modbus-exporter/deploy.sh
 $scriptdir/fritzbox-exporter/deploy.sh
--- monitoring/prometheus/deploy.sh
+++ monitoring/prometheus/deploy.sh
@@ -3,29 +3,10 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-# assumes that helm repo was updated
-kube_prometheus_stack_version=51.2.0
-prom_operator_version=$(helm search repo prometheus-community/kube-prometheus-stack --version "${kube_prometheus_stack_version}" -o json | jq -r '.[].app_version')
-if [ -z "${prom_operator_version}" ]
-then
-  echo "Did not find kube-prometheus-stack version ${kube_prometheus_stack_version}. Forgot to update helm repo?"
-  exit 1
-fi
-# update prometheus-operator CRDs
-kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${prom_operator_version}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml
-kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${prom_operator_version}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
-kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${prom_operator_version}/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
-kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${prom_operator_version}/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml
-kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${prom_operator_version}/example/prometheus-operator-crd/monitoring.coreos.com_prometheusagents.yaml
-kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${prom_operator_version}/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
-kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${prom_operator_version}/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
-kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${prom_operator_version}/example/prometheus-operator-crd/monitoring.coreos.com_scrapeconfigs.yaml
-kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${prom_operator_version}/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
-kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${prom_operator_version}/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
-
 # upgrade kube-prometheus-stack installations
-helm upgrade --install --create-namespace --namespace monitoring prometheus prometheus-community/kube-prometheus-stack --version ${kube_prometheus_stack_version} --values $scriptdir/kube-prometheus-stack-values.yaml
-helm upgrade --install --namespace monitoring prometheus-lt prometheus-community/kube-prometheus-stack --version ${kube_prometheus_stack_version} --values $scriptdir/kube-prometheus-stack-values-lt.yaml
+kubectl apply -f $scriptdir/prometheus-operator-crds.yaml
+kubectl apply -f $scriptdir/prometheus.yaml
+kubectl apply -f $scriptdir/prometheus-lt.yaml
 
 # alertmanager config and secrets
 kubectl apply --namespace monitoring -f $scriptdir/secrets.yaml
