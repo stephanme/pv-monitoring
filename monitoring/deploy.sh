04db53957eb9c54b0fda1a823d8d61dd7bcc8ed7
Stephan Merker <merker.stephan@gmail.com>
2023-10-27 19:52:41 +0200

    Deploy blackbox-exporter
    Monitors pvcontrol app and dnsmasq

--- monitoring/deploy.sh
+++ monitoring/deploy.sh
@@ -8,4 +8,5 @@ kubectl apply -f $scriptdir/namespace.yaml
 $scriptdir/prometheus/deploy.sh
 $scriptdir/modbus-exporter/deploy.sh
 $scriptdir/fritzbox-exporter/deploy.sh
+$scriptdir/blackbox-exporter/deploy.sh
 $scriptdir/pvcontrol/deploy.sh
