ba04ea8fe07bb711f28bc3697850a6396bc2ab86
Stephan Merker <merker.stephan@gmail.com>
2024-06-10 19:44:07 +0200

    Deploy paperless-ngx manually
    - replaced helm chart by k8s manifests to gain more control
    - helm chart is not that well maintained (slow updates)
    - helm chart doesn't allow separate worker deployments (future)
    - no tika, consume pvc using smb and traefik ingress in helm chart
    - redis helm chart is still used (seems well maintained)

--- paperless/deploy.sh
+++ paperless/deploy.sh
@@ -4,5 +4,8 @@ set -ex
 scriptdir=$(dirname "$0")
 kubectl apply -f $scriptdir/namespace.yaml
 kubectl apply -f $scriptdir/secrets.yaml
-kubectl apply -f $scriptdir/paperless.yaml
+kubectl apply -f $scriptdir/pvc.yaml
+kubectl apply -f $scriptdir/redis.yaml
+kubectl apply -f $scriptdir/paperless-ngx.yaml
 kubectl apply -f $scriptdir/tika.yaml
+kubectl apply -f $scriptdir/ingress.yaml
