539f056dfe4c073509ce72c7f31bf7c990ae34d2
Stephan Merker <merker.stephan@gmail.com>
2025-01-10 23:59:43 +0100

    Renovate rule to update CRD in parallel to helm chart version

--- kube-system/node-feature-discovery/deploy.sh
+++ kube-system/node-feature-discovery/deploy.sh
@@ -3,6 +3,6 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-# TODO: renovate, version must match helm chart
+# renovate: HelmChartCRD repo=https://kubernetes-sigs.github.io/node-feature-discovery/charts chart=node-feature-discovery
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/node-feature-discovery/v0.17.0/deployment/base/nfd-crds/nfd-api-crds.yaml
 kubectl apply -f $scriptdir
