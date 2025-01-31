7fc647e2cde7fc9b12d6ecff685ecb4f54b5f8ab
Stephan Merker <merker.stephan@gmail.com>
2025-01-31 18:42:38 +0100

    Add system-upgrade-controller CRD

--- system-upgrade/deploy.sh
+++ system-upgrade/deploy.sh
@@ -3,5 +3,7 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/v0.14.2/system-upgrade-controller.yaml
+version=v0.14.2
+kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/${version}/crd.yaml
+kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/${version}/system-upgrade-controller.yaml
 kubectl apply --namespace system-upgrade -f $scriptdir/k3s-upgrade.yaml
