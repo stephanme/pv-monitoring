fb12218640a69f6dbfb6a3b7e712107a8c1d8790
Stephan Merker <merker.stephan@gmail.com>
2025-07-14 08:52:51 +0200

    Prefetch HA images on pi2
    - was missing
    - even with spegel: need to prefetch in case pi1 goes offline

--- homeassistant/deploy.sh
+++ homeassistant/deploy.sh
@@ -8,6 +8,7 @@ kubectl create namespace homeassistant --dry-run=client -o yaml | kubectl apply 
 kubectl replace --force -k $scriptdir/fetch-images
 kubectl wait --for=condition=complete --timeout 30m --namespace homeassistant job/fetch-images-nasbox
 kubectl wait --for=condition=complete --timeout 30m --namespace homeassistant job/fetch-images-pi1
+kubectl wait --for=condition=complete --timeout 30m --namespace homeassistant job/fetch-images-pi2
 
 kubectl apply -k $scriptdir/homeassistant
 kubectl apply --namespace homeassistant -f $scriptdir
