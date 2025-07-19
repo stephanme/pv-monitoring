1199a7114b492d59f70313284e2c6b08e12c1f5f
Stephan Merker <merker.stephan@gmail.com>
2025-07-19 18:25:13 +0200

    Use zot for pvcontrol to reduce downtime
    - explicit image registry configuration
    - pre-fetch image using HEAD request in deploy.sh
    - run descheduler for preferredDuringSchedulingIgnoredDuringExecution only every 24h
      - otherwise pvcontrol is restarted every minute when pi1 is down

--- monitoring/pvcontrol/deploy.sh
+++ monitoring/pvcontrol/deploy.sh
@@ -2,4 +2,7 @@
 set -ex
 
 scriptdir=$(dirname "$0")
+
+# pre-fetch image to minimize downtime
+curl --silent --show-error --fail --head 'http://registry.fritz.box/v2/ghcr.io/stephanme/pv-control/manifests/latest'
 kubectl apply --namespace monitoring -f $scriptdir
