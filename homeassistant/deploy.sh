015c43e7110a8d5053fdbc8b9bfcf3af5e1982db
Stephan Merker <merker.stephan@gmail.com>
2025-07-25 16:29:26 +0200

    Use zot for HA images
    - remove image pre-fetch jobs from HA
    - remove complex kustomize setup (not needed anymore)
    - specify namespace consistently in manifests
    - namepace creation per manifest and not cmd line magic

--- homeassistant/deploy.sh
+++ homeassistant/deploy.sh
@@ -3,12 +3,5 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-kubectl create namespace homeassistant --dry-run=client -o yaml | kubectl apply -f -
-
-kubectl replace --force -k $scriptdir/fetch-images
-kubectl wait --for=condition=complete --timeout 30m --namespace homeassistant job/fetch-images-nasbox
-kubectl wait --for=condition=complete --timeout 30m --namespace homeassistant job/fetch-images-pi1
-kubectl wait --for=condition=complete --timeout 30m --namespace homeassistant job/fetch-images-pi2
-
-kubectl apply -k $scriptdir/homeassistant
-kubectl apply --namespace homeassistant -f $scriptdir
+kubectl apply -f namespace.yaml
+kubectl apply -f $scriptdir
