ecfb8dde68609a3168a884077ddddebf5d2b7417
Stephan Merker <merker.stephan@gmail.com>
2025-07-31 19:29:25 +0200

    Use cronjob for prefetching zot image
    - runs every 6d to ensure the images is GCed

--- zot/prefetch-image/deploy.sh
+++ zot/prefetch-image/deploy.sh
@@ -3,8 +3,11 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-# ensure images are pre-fetched on nodes
-kubectl replace --force -k .
-kubectl wait --for=condition=complete --timeout 30m --namespace zot job/fetch-images-nasbox
-kubectl wait --for=condition=complete --timeout 30m --namespace zot job/fetch-images-pi1
-kubectl wait --for=condition=complete --timeout 30m --namespace zot job/fetch-images-pi2
+kubectl apply -k .
+
+# ensure images are pre-fetched on nodes = trigger CronJobs manually and wait for completion
+for node in nasbox pi1 pi2; do
+  job_name="deploy-$(date +%s)-$node"
+  kubectl create job --namespace zot --from=cronjob/fetch-images-$node $job_name
+  kubectl wait --namespace zot --for=condition=complete --timeout=30m job/$job_name
+done
