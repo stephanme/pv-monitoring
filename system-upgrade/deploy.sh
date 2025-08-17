b248565d2bcb5fa41bcd3f5c8f06afe84e0a1464
Stephan Merker <merker.stephan@gmail.com>
2025-08-17 18:09:43 +0200

    Renovate k3s version

--- system-upgrade/deploy.sh
+++ system-upgrade/deploy.sh
@@ -3,7 +3,7 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-# renovate: datasource=github-release depName=rancher/system-upgrade-controller versioning=semver-coerced
+# renovate: datasource=github-releases depName=rancher/system-upgrade-controller versioning=semver-coerced
 SUC_VERSION=v0.16.2
 kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/${SUC_VERSION}/crd.yaml
 kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/${SUC_VERSION}/system-upgrade-controller.yaml
