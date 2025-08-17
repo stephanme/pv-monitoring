d03c44917dc3eae9cd29dfd9d99c1dff95a82428
Stephan Merker <merker.stephan@gmail.com>
2025-08-17 17:37:56 +0200

    Renovate k3s version
    - k3s version
    - system-upgrade-controller version
    - remove obsolete registryAliases

--- system-upgrade/deploy.sh
+++ system-upgrade/deploy.sh
@@ -3,7 +3,8 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-version=v0.16.2
-kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/${version}/crd.yaml
-kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/${version}/system-upgrade-controller.yaml
+# renovate: datasource=github-releases depName=rancher/system-upgrade-controller versioning=semver-coerced
+SUC_VERSION=v0.16.2
+kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/${SUC_VERSION}/crd.yaml
+kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/${SUC_VERSION}/system-upgrade-controller.yaml
 kubectl apply --namespace system-upgrade -f $scriptdir/k3s-upgrade.yaml
