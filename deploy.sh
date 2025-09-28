a8480fe5800460ae7d13ec1bfcdcbded8a938782
Stephan Merker <merker.stephan@gmail.com>
2025-09-28 22:56:32 +0200

    Add kopia server for backups
    - namespace: kopia
    - backups on md0 as backup user

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,5 @@ $scriptdir/monitoring/deploy.sh
 $scriptdir/pvcontrol/deploy.sh
 $scriptdir/homeassistant/deploy.sh
 $scriptdir/paperless/deploy.sh
+$scriptdir/kopia/deploy.sh
 $scriptdir/default/deploy.sh
