3a312c8e0b7aeae91e5d709f69ab07f827d5f094
Stephan Merker <merker.stephan@gmail.com>
2025-10-17 22:20:26 +0200

    dnsmasq: use kustomize
    - automatic dnsmasq restart on config change

--- kube-system/dnsmasq/deploy.sh
+++ kube-system/dnsmasq/deploy.sh
@@ -2,4 +2,5 @@
 set -ex
 
 scriptdir=$(dirname "$0")
-kubectl apply -f $scriptdir
+kubectl apply -f $scriptdir/coredns.yaml
+kubectl apply --prune --prune-allowlist=core/v1/ConfigMap -l app=dnsmasq -k $scriptdir
