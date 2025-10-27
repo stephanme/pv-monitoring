8222d2a0b4b09e27bad36418bfcb3335d577876e
Stephan Merker <merker.stephan@gmail.com>
2025-10-27 18:26:32 +0100

    Add headlamp

--- kube-system/deploy.sh
+++ kube-system/deploy.sh
@@ -6,3 +6,6 @@ scriptdir=$(dirname "$0")
 $scriptdir/metallb/deploy.sh
 $scriptdir/traefik/deploy.sh
 $scriptdir/dnsmasq/deploy.sh
+$scriptdir/node-feature-discovery/deploy.sh
+$scriptdir/descheduler/deploy.sh
+$scriptdir/headlamp/deploy.sh
