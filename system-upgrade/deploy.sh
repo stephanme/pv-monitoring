5b136a83703c9e87c1e4efd61f8929aa88f0fb0e
Stephan Merker <merker.stephan@gmail.com>
2026-02-15 15:57:39 +0100

    Use longer deadline for k3s system upgrade
    - 30m because volume replica rebuilding takes often longer than 15m
    - convert deploy script magic into kustomization
    - update/fix regsync config

--- system-upgrade/deploy.sh
+++ system-upgrade/deploy.sh
@@ -3,8 +3,4 @@ set -ex
 
 scriptdir=$(dirname "$0")
 
-# renovate: datasource=github-releases depName=rancher/system-upgrade-controller versioning=semver-coerced
-SUC_VERSION=v0.18.0
-kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/${SUC_VERSION}/crd.yaml
-kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/download/${SUC_VERSION}/system-upgrade-controller.yaml
-kubectl apply -f $scriptdir/k3s-upgrade.yaml
+kubectl apply -k $scriptdir
